3d5ca5051c8c17057fbf84212ab2bbb13501e896
sachix1001 <sachix1001@gmail.com>
2020-06-11 12:58:12 +1000

    base test run

---
+++ day1/e2etests/test.js
@@ -0,0 +1,19 @@
+const expect = require("chai").expect;
+
+describe("TodoList App", () => {
+  it("Should load with the right title", () => {
+    browser.url("http://localhost:3000/");
+    const actualTitle = browser.getTitle();
+
+    expect(actualTitle).to.eql("Todo List");
+  });
+  it("Should allow me to create a Todo", () => {
+    const todoText = "Get better at testing";
+    browser.url("http://localhost:3000/");
+    browser.findElement(by.css(".todo-input")).setValue(todoText);
+    browser.click(".todo-submit");
+    const actual = browser.element(".todo-text").getText();
+
+    expect(actual).to.equal(todoText);
+  });
+});
